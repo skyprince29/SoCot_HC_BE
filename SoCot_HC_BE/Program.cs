6d1bcf76f2bc933cc74ea4ac0b02a78ad5b38331
Jay Tee C. Condesa <161785979+jtcondesa@users.noreply.github.com>
2025-06-11 08:27:57 +0800

    signalR update program.cs

--- SoCot_HC_BE/Program.cs
+++ SoCot_HC_BE/Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigins",
         policy =>
         {
-         policy.WithOrigins()
+         policy.WithOrigins("https://localhost:44319")
             .AllowAnyMethod()    // Crucial for OPTIONS preflight, POST, GET, etc.
             .AllowAnyHeader()    // Allows any headers (e.g., Content-Type, Authorization)
             .AllowCredentials(); // REQUIRED for SignalR (and if you send cookies/auth tokens)
