ee27fafe281429a6156665aba7dc4247a42b0cf7
John Kyle Lastimosa <johnkyle.lastimosa99@gmail.com>
2025-06-20 14:36:24 +0800

    0022: Update module status flow of patient registry and patient department transaction

--- SoCot_HC_BE/Scripts/Insert/12. insert_module_status_flow.sql
+++ SoCot_HC_BE/Scripts/Insert/12. insert_module_status_flow.sql
@@ -11,25 +11,20 @@ VALUES
     (3, 1, 4, 6, 0, 1),
     -- From Arrived (4) to Discharged (7)
     (4, 1, 4, 7, 0, 1),
-    -- From Arrived (4) to Cancelled (3)
-    (5, 1, 4, 3, 0, 1),
+
 
      -- Status flow for PatientDepartmentTransaction
     -- From NULL to Pending
-    (6, 2, NULL, 8, 1, 0),
+    (5, 2, NULL, 8, 1, 0),
     -- From Pending (8) to On-going (9)
-    (7, 2, 8, 9, 0, 0),
-    -- From Pending (8) to Cancelled (3)
-    (8, 2, 8, 3, 0, 1),
+    (6, 2, 8, 3, 0, 1),
     -- From On-going (9) to Deferred (11)
-    (9, 2, 9, 11, 0, 0),
-    -- From On-going (9) to Cancelled (3)
-    (10, 2, 9, 3, 0, 1),
+    (7, 2, 9, 11, 0, 0),
+    -- From On-going (9) to Redirected (5)
+    (8, 2, 9, 5, 0, 1),
     -- From On-going (9) to Cleared (10)
-    (11, 2, 9, 10, 0, 1),
-    -- From Deferred (11) to Cancelled (3)
-    (12, 2, 11, 3, 0, 1),
-    -- From Deferred (11) to Cleared (10)
-    (13, 2, 11, 10, 0, 1);
+    (9, 2, 9, 10, 0, 1),
+    -- From Deferred (11) to On-going (9)
+    (10, 2, 11, 9, 0, 0);
     -- insert other following Status flow below
 SET IDENTITY_INSERT ModuleStatusFlow OFF;
